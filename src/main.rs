5797145c227046805d4bfb877fd750bbdc11f2fe
eengamer2007 <61841788+eengamer2007@users.noreply.github.com>
2021-11-16 18:28:53 +0100

    did main

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ mod help;
 mod day_4;
 mod day_5;
 mod day_6;
+mod day_7;
+mod day_8;
 
 #[allow(dead_code)]
 
 fn main() {
     pretty_env_logger::init();
-    println!("{}",day_6::part_1());
+    println!("{}",day_8::part_2());
 }
