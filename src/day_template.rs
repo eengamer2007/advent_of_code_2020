34df23533213824233dc4edd5f98b0ed15b06e79
eengamer2007 <61841788+eengamer2007@users.noreply.github.com>
2021-11-16 18:28:24 +0100

    fixed template

---
+++ src/day_template.rs
@@ -0,0 +1,11 @@
+use help;
+
+#[allow(dead_code)]
+pub fn part_1(){
+    let input: [..] = help::read().unwrap();
+}
+
+#[allow(dead_code)]
+pub fn part_2(){
+    let input: Vec<String> = help::read().unwrap();
+}
